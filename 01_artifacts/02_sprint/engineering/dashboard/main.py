a8bc70c9ab54cba4f6c1c08a9b4c5b7a6cbdf65b
boringeugene <sharovzeny@gmail.com>
2022-10-25 10:18:49 +0300

    added tooltip_data update new title and labels

--- 01_artifacts/02_sprint/engineering/dashboard/main.py
+++ 01_artifacts/02_sprint/engineering/dashboard/main.py
@@ -3,6 +3,7 @@ from dash import Dash, dash_table, html, dcc
 from dash.dependencies import Input, Output
 import pandas as pd
 
+# подключение к базе данных со статьями
 engine = sqlalchemy.create_engine(url="postgresql://mdb:Gsdbe4k754ghmf@185.87.50.149:6432/dblp")
 con = engine.connect()
 
@@ -16,11 +17,12 @@ app = Dash(__name__)
 
 PAGE_SIZE = 15
 
-
+# описание веб интерфейса
 app.layout = html.Div([
-    html.H6("Write the text you want to search for!"),
+    html.H1("Citation network project"),
+    html.H3("Write the title you are looking for:"),
     html.Div([
-        "Input: ",
+        "Search: ",
         dcc.Input(id='my-input', value='study', type='text')
     ]),
     html.Br(),
@@ -30,9 +32,9 @@ app.layout = html.Div([
             {"name": i, "id": i} for i in df.columns
         ],
         style_data={
-                'whiteSpace': 'normal',
-                'lineWidth': '10px'
-            },
+            'whiteSpace': 'normal',
+            'lineWidth': '10px'
+        },
         css=[{
             'selector': '.dash-spreadsheet td div',
             'rule': '''
@@ -55,16 +57,27 @@ app.layout = html.Div([
 
 ])
 
+
 @app.callback(
     Output('datatable-paging', 'data'),
+    Output('datatable-paging', 'tooltip_data'),
     Input('datatable-paging', "page_current"),
     Input('datatable-paging', "page_size"),
     Input('my-input', 'value')
 )
 def update_table(page_current, page_size, pattern):
-    return df.loc[df['title'].str.contains(pattern)].iloc[
-        page_current*page_size:(page_current+ 1)*page_size
-    ].to_dict('records')
+    # фильтрация данных для одной странице по ключу pattern
+    # поиск происходит только по полному совпадению в колонке 'title'
+    new_data = df.loc[df['title'].str.contains(pattern)].iloc[
+               page_current * page_size:(page_current + 1) * page_size
+               ]
+    new_tooltip_data = [
+        {column: {'value': str(value), 'type': 'markdown'}
+         for column, value in row.items()
+         } for row in new_data.to_dict('records')
+    ]
+    return new_data.to_dict('records'), new_tooltip_data
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
