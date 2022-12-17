238c88c83b12d36a3eaeeb7ca34999c0840de117
boringeugene <sharovzeny@gmail.com>
2022-12-17 09:27:06 +0300

    modified get_server_function to fit server response

--- 01_artifacts/05_sprint/client.py
+++ 01_artifacts/05_sprint/client.py
@@ -1,4 +1,5 @@
 import requests
+import numpy as np
 from typing import List
 import pandas as pd
 from loguru import logger
@@ -6,17 +7,19 @@ from loguru import logger
 SERVER_IP_PORT = '127.0.0.1:8000'
 
 
-def get_server_prediction(data_to_predict: pd.DataFrame) -> List[float]:
+def get_server_prediction(data_to_predict: pd.DataFrame) -> List[str]:
     """
     This function sends request to server for data prediction and returns
     prediction results.
     """
-    data = data_to_predict.astype(str).values.tolist()
-    # resp = requests.post(f'http://{SERVER_IP_PORT}/predict',
-    #                     json={'data': data, 'features': list(data_to_predict.columns)})
-
+    text = data_to_predict.dropna(subset=['abstract'])['abstract'].tolist()
+    resp = requests.post(f'http://{SERVER_IP_PORT}/predict',
+                         json={'text': text})
+    recommended_ids = []
+    for recommendation_list in resp.json()['response']:
+        recommended_ids.extend(recommendation_list)
     # ниже находится тестовый вариант
-    return list(data_to_predict.sample(min(5, len(data_to_predict))).index)
+    return recommended_ids
 
 
 if __name__ == '__main__':
